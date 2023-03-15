b5a7739f4bdf8b0c5b7d63a0a0537e5896e18c90
Replit user <>
2023-03-15 00:17:57 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -130,7 +130,7 @@ sumar (a1,a2)+(b1+b2). Almacene el resultado en el vector c.
 */
 void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
   int capacidadVector = c->capacidad;
-  int = 0
+  int i = 0
   while (i < capacidadVector)
     {
       c->datos[i] = a1 + a2;
